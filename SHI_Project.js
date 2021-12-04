9307e344345f651585e54de9fe7aba9d0b371392
amrik <73204325+amrikverma@users.noreply.github.com>
2021-12-03 20:43:31 -0800

    js file added

---
+++ SHI_Project.js
@@ -0,0 +1,25 @@
+//project medium:
+var output = ""
+var contextoutput = ""
+var history = "Slideshow or Poster (with oral presentation)"
+var science = "Essay or Slideshow (with oral presentation and provided research)"
+var math = "Poster or Oral Presentation"
+var design = "Image Slideshow or Model"
+var art = "Artpeice and Discription, or Model and supporting text"
+var className = document.getElementById('subjectButton').value
+if(className == "history"){
+output = history
+}
+if(className == "science"){
+output = science
+}
+if(classname == "math"){
+output = math
+}
+if(className == "design"){
+output = design
+}
+if(className == "art"){
+output = art
+}
+document.getElementById("outputText").innerHTML = output;
