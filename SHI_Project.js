bf9d453726b0f32e739c97af047bfd1eef2afee5
amrik <73204325+amrikverma@users.noreply.github.com>
2021-12-11 17:44:01 -0800

    subject project button test

--- SHI_Project.js
+++ SHI_Project.js
@@ -1,25 +1,30 @@
 //project medium:
-var output = ""
-var contextoutput = ""
-var history = "Slideshow or Poster (with oral presentation)"
-var science = "Essay or Slideshow (with oral presentation and provided research)"
-var math = "Poster or Oral Presentation"
-var design = "Image Slideshow or Model"
-var art = "Artpeice and Discription, or Model and supporting text"
-var className = document.getElementById('subjectButton').value
-if(className == "history"){
-    output = history
+// var output = ""
+// var contextoutput = ""
+// var history = "Slideshow or Poster (with oral presentation)"
+// var science = "Essay or Slideshow (with oral presentation and provided research)"
+// var math = "Poster or Oral Presentation"
+// var design = "Image Slideshow or Model"
+// var art = "Artpeice and Discription, or Model and supporting text"
+// var className = document.getElementById('subjectButton').value
+// if(className == "history"){
+//     output = history
+// }
+// if(className == "science"){
+//     output = science
+// }
+// if(className == "math"){
+//     output = math
+// }
+// if(className == "design"){
+//     output = design
+// }
+// if(className == "art"){
+//     output = art
+// }
+// document.getElementById("outputText").innerHTML = output;
+function onSubjectSubmit() {
+    event.preventDefault();
+    var subjectSubmitText = document.getElementById("subject").value; 
+    alert("Text input: " + subjectSubmitText)
 }
-if(className == "science"){
-    output = science
-}
-if(className == "math"){
-    output = math
-}
-if(className == "design"){
-    output = design
-}
-if(className == "art"){
-    output = art
-}
-document.getElementById("outputText").innerHTML = output;
