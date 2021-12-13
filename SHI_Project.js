fcf2981e8b11d8ab1de331ab5436af79b47f7e5b
Hurricane2010 <83786779+Hurricane2010@users.noreply.github.com>
2021-12-13 14:51:41 -0800

    updating JS for project medum file

--- SHI_Project.js
+++ SHI_Project.js
@@ -1,30 +1,28 @@
-//project medium:
-// var output = ""
-// var contextoutput = ""
-// var history = "Slideshow or Poster (with oral presentation)"
-// var science = "Essay or Slideshow (with oral presentation and provided research)"
-// var math = "Poster or Oral Presentation"
-// var design = "Image Slideshow or Model"
-// var art = "Artpeice and Discription, or Model and supporting text"
-// var className = document.getElementById('subjectButton').value
-// if(className == "history"){
-//     output = history
-// }
-// if(className == "science"){
-//     output = science
-// }
-// if(className == "math"){
-//     output = math
-// }
-// if(className == "design"){
-//     output = design
-// }
-// if(className == "art"){
-//     output = art
-// }
-// document.getElementById("outputText").innerHTML = output;
+var output = ""
+var contextoutput = ""
+var history = "Slideshow or Poster (with oral presentation)"
+var science = "Essay or Slideshow (with oral presentation and provided research)"
+var math = "Poster or Oral Presentation"
+var design = "Image Slideshow or Model"
+var art = "Artpeice and Discription, or Model and supporting text"
 function onSubjectSubmit() {
     event.preventDefault();
     var subjectSubmitText = document.getElementById("subject").value; 
-    alert("Text input: " + subjectSubmitText)
+    //alert("Text input: " + subjectSubmitText)
+    if(subjectSubmitText.toLowerCase() = "history"){
+        output = history;
+    }
+    if(subjectSubmitText.toLowerCase() = "science"){
+        output = science;
+    }
+    if(subjectSubmitText.toLowerCase() = "math"){
+        output = math;
+    }
+    if(subjectSubmitText.toLowerCase() = "design"){
+        output = design;
+    }
+    if(subjectSubmitText.toLowerCase() = "art"){
+        output = art;
+    }
+    document.getElementById('mediumResult').innerHTML = output;
 }
