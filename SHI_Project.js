5c3cfeee0e2e34078a4ca563333f0a2f704a7997
Hurricane2010 <83786779+Hurricane2010@users.noreply.github.com>
2021-12-14 19:20:21 -0800

    js update (again lol)

--- SHI_Project.js
+++ SHI_Project.js
@@ -1,12 +1,27 @@
-var output = ""
-var contextoutput = ""
-var history = "Slideshow or Poster (with oral presentation)"
-var science = "Essay or Slideshow (with oral presentation and provided research)"
-var math = "Poster or Oral Presentation"
-var design = "Image Slideshow or Model"
-var art = "Artpeice and Discription, or Model and supporting text"
+var output = "";
+var contextoutput = "";
+var socStu = "Slideshow or Poster (with oral presentation)";
+var science = "Essay or Slideshow (with oral presentation and provided research)";
+var math = "Poster or Oral Presentation";
+var design = "Image Slideshow or Model";
+var art = "Artpeice and Discription, or Model and supporting text";
 function onSubjectSubmit() {
     event.preventDefault();
     var subjectSubmitText = document.getElementById("subject").value; 
-    alert("Text input: " + subjectSubmitText)
+    //alert("Text input: " + subjectSubmitText)
+    if(subjectSubmitText == "history"){
+        document.getElementById("outputText").innerHTML = socStu;
+    }
+    if(subjectSubmitText == "science"){
+        document.getElementById("outputText").innerHTML = science;
+    }
+    if(subjectSubmitText == "math"){
+        document.getElementById("outputText").innerHTML = math;
+    }
+    if(subjectSubmitText == "design"){
+        document.getElementById("outputText").innerHTML = design;
+    }
+    if(subjectSubmitText == "art"){
+        document.getElementById("outputText").innerHTML = art;
+    }
 }
