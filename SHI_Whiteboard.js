0b674699efdf9159950350939772f5c033b4dd7b
Hurricane2010 <83786779+Hurricane2010@users.noreply.github.com>
2022-01-04 16:37:18 -0800

    fixed whiteboard

--- SHI_Whiteboard.js
+++ SHI_Whiteboard.js
@@ -1,33 +1,58 @@
-const ctx = document.getElementById('canvas').getContext('2D');
-window.addEventListener('resize', resize);
-resize();
-let mousePos = {
-    x:0,
-    y:0
-}
-window.addEventListener("mousemove", draw);
-window.addEventListener("mousedown",mousePosition);
-window.addEventListener("mouseenter", mousePosition);
+window.addEventListener("load", () => {
+    const canvas = document.querySelector('#canvas');
+    const ctx = canvas.getContext('2d');
+    const lineWidth = 3
 
-function mousePosition(e){
-    mousePos.x = e.clientx;
-    mousePos.y = e.clienty;
-}
-function resize(){
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-}
+    //Resizing
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
 
-function draw(e){
-    if(e.buttons !== 1){
-        return;
+    //Variables
+    let painting = false;
+    function startPosition(e){
+        painting = true;
+        draw(e);
     }
-    ctx.beginPath();
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = '#111';
-    ctx.linewidth = 5;
-    ctx.moveTo(mousepos.x, mousepos.y);
-    mousePosition(e);
-    ctx.lineTo(mousePos.x, mousePos.y);
-    ctx.stroke();
-}
+
+    function finishedPosition(){
+        painting = false;
+        ctx.beginPath();
+    }
+
+    function draw(e){
+        if(!painting) return;
+        ctx.lineWidth = lineWidth;
+        ctx.lineCap = 'round'
+
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+
+    }
+
+    //EventListeners
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
+
+
+    //Button Functions
+    function lineWidthIncreaese(){
+        event.preventDefault();
+        lineWidth = lineWidth + 1;
+    }
+    
+    function lineWidthDecrease(){
+        event.preventDefault();
+        lineWidth = lineWidth - 1;
+    }
+});
+
+
+window.addEventListener('resize', () =>{
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+});
+
+
