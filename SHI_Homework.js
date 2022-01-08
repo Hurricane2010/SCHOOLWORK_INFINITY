def8410b9bd209ad55f34c6e5e57cc8f6fca5862
amrik <73204325+amrikverma@users.noreply.github.com>
2022-01-07 20:52:21 -0800

    calc button update again lol

--- SHI_Homework.js
+++ SHI_Homework.js
@@ -3,3 +3,30 @@ function onHomeworkSubmit() {
     var hwSubmitText = document.getElementById("subject_hw").value;
     alert(hwSubmitText);
 }
+
+//Calculator:
+function calculator() 
+{
+    document.getElementById("calcdisplay").style.display="block";
+}
+
+//function that display value
+function dis(val)
+{
+    document.getElementById("result").value+=val
+}
+
+//function that evaluates the digit and return result
+function solve()
+{
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
+
+//function that clear the display
+function clr()
+{
+    document.getElementById("result").value = ""
+}
+
