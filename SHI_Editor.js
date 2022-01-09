d02e18dd278e3c558b2923f1f10d6bf013546219
amrik <73204325+amrikverma@users.noreply.github.com>
2022-01-08 22:06:30 -0800

    Font size thing but its not very good cuz its unfinished

--- SHI_Editor.js
+++ SHI_Editor.js
@@ -13,3 +13,10 @@ function setUrl() {
     document.execCommand('insertHTML', false, '<a href="' + url + '" target="_blank">' + sText + '</a>');
     document.getElementById('txtFormatUrl').value = '';
 }
+
+function setSize() {
+    var sizeNum = document.getElementById('txtSetSize').value;
+    var sText = document.getSelection();
+    document.execCommand('insertHTML', false, '<p style="font-size:' + sizeNum + 'px">' + sText + '</p>');
+    document.getElementById('txtSetSize').value = '';
+}
