097c0e4a6dbc1dcc0fd97809bd18925939e44253
Hurricane2010 <83786779+Hurricane2010@users.noreply.github.com>
2022-02-15 20:08:57 -0800

    created video appearance

---
+++ Content.js
@@ -0,0 +1,5 @@
+function GoogleVideos(){
+    document.getElementById("Google_Content").style.visibility = "visible";
+    document.getElementById("content_access").style.visibility = "collapse";
+}
+
