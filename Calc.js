8b78ee6192f11e8f2a7730099ede56723188e563
amrik <73204325+amrikverma@users.noreply.github.com>
2022-02-20 22:21:14 -0800

    calc update

--- Calc.js
+++ Calc.js
@@ -2,8 +2,3 @@ function graphCalc() {
     var div = document.getElementById("graphDesmos");
     div.style.display = div.style.display == "none" ? "block" : "none";
 }
-
-function togglediv(id) {
-    var div = document.getElementById(id);
-    div.style.display = div.style.display == "none" ? "block" : "none";
-}
