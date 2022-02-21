1d98ed2bec839cd7a14505c2c0d8d8fb7af9102b
Hurricane2010 <83786779+Hurricane2010@users.noreply.github.com>
2022-02-20 20:11:00 -0800

    idfix

--- Content.js
+++ Content.js
@@ -1,5 +1,5 @@
 function GoogleVideos(){
     document.getElementById("Google_Content").style.visibility = "visible";
-    document.getElementById("content_access").style.visibility = "collapse";
+    document.getElementById("content_access_google").style.visibility = "collapse";
 }
 
