7572fd5c72bfdd29f62758fe066abe84c9254d30
amrik <73204325+amrikverma@users.noreply.github.com>
2022-02-21 00:41:39 -0800

    calculator - geo calc in progress

--- Calc.js
+++ Calc.js
@@ -1,4 +1,14 @@
+function standCalc() {
+    var div = document.getElementById("sciCalc");
+    div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
 function graphCalc() {
     var div = document.getElementById("graphDesmos");
+    div.style.display = div.style.display == "none" ? "block" : "none";
+}
+
+function geoCalc() {
+    var div = document.getElementById("geoCalc");
     div.style.display = div.style.display == "none" ? "block" : "none";
 }
